e0282c65a439d317da62febb75f3f4b3faca577b
Diamondyuan <admin@diamondyuan.com>
2018-10-27 04:01:55 +0800

    fix: 修改 src/services/common/contentScripttool/index.ts 内文件名大小写错误，以及 webpack/chromeManifestPlugin.js

--- webpack/chromeManifestPlugin.js
+++ webpack/chromeManifestPlugin.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path');
 
 const writeFile = (path, data, opts = 'utf8') =>
   new Promise((res, rej) => {
@@ -16,6 +17,17 @@ const readFile = (path, opts = 'utf8') =>
     });
   });
 
+const mkdirsSync = (dirname) => {
+  if (fs.existsSync(dirname)) {
+    return true;
+  } else {
+    if (mkdirsSync(path.dirname(dirname))) {
+      fs.mkdirSync(dirname);
+      return true;
+    }
+  }
+};
+
 class ChromeManifestPlugin {
   constructor(options) {
     this.options = options;
@@ -27,6 +39,7 @@ class ChromeManifestPlugin {
       const manifest = packageJson.chrome;
       manifest.version = packageJson.version;
       manifest.description = packageJson.description;
+      mkdirsSync(path.dirname(this.options.out));
       await writeFile(this.options.out, JSON.stringify(manifest));
     });
 
