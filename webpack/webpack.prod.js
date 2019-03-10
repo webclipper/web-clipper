8cae14d3b025cda00aae1c6bd3fbd6c9435785c5
DiamondYuan <admin@diamondyuan.com>
2019-03-10 22:06:23 +0800

    chore(clipper): disable minimize when build

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,22 +1,11 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = common.map(config => {
   return merge(config, {
     mode: 'production',
     optimization: {
-      minimizer: [
-        new UglifyJSPlugin({
-          uglifyOptions: {
-            compress: {
-              warnings: false,
-              drop_debugger: true,
-              drop_console: true
-            }
-          }
-        })
-      ]
+      minimize: false
     }
   });
 });
