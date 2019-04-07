6aa0422098ab0547d9ff1f84029a45a6353cb516
DiamondYuan <admin@diamondyuan.com>
2019-04-07 11:31:31 +0800

    fix(ci): fix release dest path

--- script/travis_release.js
+++ script/travis_release.js
@@ -1,6 +1,6 @@
 const compressing = require('compressing');
 const path = require('path');
 
-const dest = path.resolve(homedir, `../yuquewebclipper.zip`);
+const dest = path.resolve(__dirname, `../yuquewebclipper.zip`);
 const source = path.resolve(__dirname, `../dist`);
 compressing.zip.compressDir(source, dest);
