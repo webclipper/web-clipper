937cd6bb17dd7485014561e1b7c2588d3e58ca37
DiamondYuan <admin@diamondyuan.com>
2019-06-20 22:31:58 +0800

    feat: change style of tool

--- src/components/section/index.tsx
+++ src/components/section/index.tsx
@@ -1,21 +1,18 @@
 import * as React from 'react';
-import * as styles from './index.scss';
 import { Divider } from 'antd';
+import styles from './index.scss';
 
 interface Props {
   title?: string;
   line?: boolean;
 }
 
-export default class Section extends React.Component<Props> {
-  public render() {
-    const { title, line = false } = this.props;
-    return (
-      <div className={styles.section}>
-        {line && <Divider className={styles.divider} />}
-        {title && <h1 className={styles.sectionTitle}>{title}</h1>}
-        {this.props.children}
-      </div>
-    );
-  }
-}
+const Section: React.FC<Props> = ({ line = false, title, children }) => (
+  <div className={styles.section}>
+    {line && <Divider style={{ marginBottom: 16 }} />}
+    {title && <h1 className={styles.sectionTitle}>{title}</h1>}
+    {children}
+  </div>
+);
+
+export default Section;
--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import * as ReactDOM from 'react-dom';
 import * as styles from './app.scss';
 import Complete from './complete/complete';
