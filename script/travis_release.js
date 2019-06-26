d5d1a9551ed060a6023e7c4b1f143db55c523f92
DiamondYuan <admin@diamondyuan.com>
2019-06-26 12:43:16 +0800

    chore: update release script

--- script/travis_release.js
+++ script/travis_release.js
@@ -1,6 +1,6 @@
 const compressing = require('compressing');
 const path = require('path');
 
-const dest = path.resolve(__dirname, `../yuquewebclipper.zip`);
+const dest = path.resolve(__dirname, `../webclipper.zip`);
 const source = path.resolve(__dirname, `../dist`);
 compressing.zip.compressDir(source, dest);
