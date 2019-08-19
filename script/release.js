840a9e988fbd5d03eff1c892e3da7b239d5cf816
DiamondYuan <admin@diamondyuan.com>
2019-08-19 22:30:11 +0800

    chore: change webpack config

--- script/release.js
+++ script/release.js
@@ -1,9 +1,18 @@
 const compressing = require('compressing');
 const path = require('path');
 const { version, name } = require('../package.json');
+const pump = require('pump');
+const fs = require('fs');
 
 const homedir = require('os').homedir();
 
 const dest = path.resolve(homedir, `${name}_${version}.zip`);
 const source = path.resolve(__dirname, `../dist`);
-compressing.zip.compressDir(source, dest);
+const zipStream = new compressing.zip.Stream();
+
+fs.readdirSync(source).forEach(o => {
+  zipStream.addEntry(path.join(source, o));
+});
+
+const destStream = fs.createWriteStream(dest);
+pump(zipStream, destStream);
