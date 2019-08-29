a46b4fe5b658696c56b4ad9942c2c6481d75354c
DiamondYuan <admin@diamondyuan.com>
2019-08-30 00:05:58 +0800

    feat: support onenote oauth

---
+++ src/common/backend/services/onenote_oauth/index.ts
@@ -0,0 +1,30 @@
+import config from '@/config';
+import { ServiceMeta } from './../interface';
+import Service from './service';
+import localeService from '@/common/locales';
+import { stringify } from 'qs';
+import form from './form';
+
+const oauthUrl = `https://login.microsoftonline.com/consumers/oauth2/v2.0/authorize?${stringify({
+  scope: 'Notes.Create User.ReadBasic.All	 offline_access',
+  client_id: config.oneNoteClientId,
+  state: chrome.runtime.id,
+  response_type: 'code',
+  response_mode: 'query',
+  redirect_uri: config.oneNoteCallBack,
+})}`;
+
+export default (): ServiceMeta => {
+  return {
+    name: localeService.format({
+      id: 'backend.services.onenote_oauth.name',
+      defaultMessage: 'OneNote',
+    }),
+    icon: 'onenote_oauth',
+    type: 'onenote_oauth',
+    service: Service,
+    oauthUrl,
+    form: form,
+    homePage: 'https://products.office.com/en-us/onenote/digital-note-taking-app',
+  };
+};
--- src/config.ts
+++ src/config.ts
@@ -3,20 +3,26 @@ interface WebClipperConfig {
   yuqueClientId: string;
   yuqueCallback: string;
   yuqueScope: string;
+  oneNoteCallBack: string;
+  oneNoteClientId: string;
 }
 
 let config: WebClipperConfig = {
   icon: 'icon.png',
   yuqueClientId: 'D1AwzCeDPLFWGfcGv7ze',
-  yuqueCallback: 'http://webclipper-oauth.yfd.im/yuque_oauth',
+  yuqueCallback: 'https://webclipper-oauth.yfd.im/yuque_oauth',
   yuqueScope: 'doc,group,repo',
+  oneNoteClientId: '',
+  oneNoteCallBack: '',
 };
 
 if (process.env.NODE_ENV === 'development') {
   config = Object.assign({}, config, {
     icon: 'icon-dev.png',
     yuqueClientId: 'fylbi7lzfNjhkfyi0hJp',
-    yuqueCallback: 'http://webclipper-oauth.test.yfd.im/yuque_oauth',
+    yuqueCallback: 'https://webclipper-oauth.test.yfd.im/yuque_oauth',
+    oneNoteClientId: '01c7500b-98dd-4f37-813f-a959382793ac',
+    oneNoteCallBack: 'http://localhost:3000/onenote_oauth',
   });
 }
 
