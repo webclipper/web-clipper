d49c881941f393c826162b24fb5b49dd1a6bb898
DiamondYuan <admin@diamondyuan.com>
2019-10-15 00:59:40 +0800

    feat: should close login tab

--- src/browser/background/index.ts
+++ src/browser/background/index.ts
@@ -1,8 +1,22 @@
 import * as browser from '@web-clipper/chrome-promise';
 import { clickIcon, doYouAliveNow } from 'browserActions/browser';
 import config from '@/config';
+import { MessageListenerCombiner } from '@web-clipper/message-listener-combiner';
+import { closeCurrentTab } from '../actions/message';
 
 browser.browserAction.setIcon({ path: config.icon });
+
+const listeners = new MessageListenerCombiner().case(
+  closeCurrentTab,
+  async (_payload, _sender, _sendResponse) => {
+    if (_sender.tab && _sender.tab.id) {
+      chrome.tabs.remove(_sender.tab.id);
+    }
+    return true;
+  }
+);
+
+browser.runtime.onMessage.addListener(listeners.handle);
 
 browser.browserAction.onClicked.addListener(async tab => {
   const tabId = tab.id;
