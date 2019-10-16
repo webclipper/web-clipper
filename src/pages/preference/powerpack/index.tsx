919bd859be5f9fe5d293881a994ef88666f01ce2
DiamondYuan <admin@diamondyuan.com>
2019-10-16 23:53:59 +0800

    feat: support powerpack login

--- src/pages/preference/powerpack/index.tsx
+++ src/pages/preference/powerpack/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import { Button } from 'antd';
+import { Button, List, Avatar } from 'antd';
 import IconFont from '@/components/IconFont';
 import { stringify } from 'qs';
 import browserId from '@/common/id';
 import config from '@/config';
+import { useSelector } from 'dva';
+import { GlobalStore, LOCAL_ACCESS_TOKEN_LOCALE_KEY } from '@/common/types';
+import dayjs from 'dayjs';
+import { localStorageService } from '@/common/chrome/storage';
 
 interface PowerpackProps {}
 
-const Powerpack: React.FC<PowerpackProps> = _props => {
+const Powerpack: React.FC<PowerpackProps> = () => {
   const githubOauthUrl = `https://github.com/login/oauth/authorize?${stringify({
     client_id: config.githubClientId,
     redirect_uri: config.githubCallback,
@@ -15,7 +19,40 @@ const Powerpack: React.FC<PowerpackProps> = _props => {
     state: browserId(),
   })}`;
 
-  return (
+  const { userInfo } = useSelector((g: GlobalStore) => ({
+    userInfo: g.userPreference.userInfo,
+  }));
+
+  return userInfo ? (
+    <div>
+      <List.Item
+        actions={[
+          <Button
+            key="logout"
+            type="link"
+            style={{ color: 'red' }}
+            onClick={() => {
+              localStorageService.set(LOCAL_ACCESS_TOKEN_LOCALE_KEY, '');
+            }}
+          >
+            Logout
+          </Button>,
+        ]}
+      >
+        <List.Item.Meta
+          avatar={<Avatar src={userInfo.avatar_url} />}
+          title={userInfo.name}
+          description={userInfo.email}
+        />
+        <div>
+          Expiry:
+          {dayjs(userInfo.expire_date)
+            .add(1, 'day')
+            .format('YYYY-MM-DD')}
+        </div>
+      </List.Item>
+    </div>
+  ) : (
     <div>
       <p>Sign Up for Powerpack</p>
       <Button href={githubOauthUrl} target="_blank">
