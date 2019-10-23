e519291db3fc710dcf097cce71dadbd46c836dc2
DiamondYuan <admin@diamondyuan.com>
2019-10-23 23:34:12 +0800

    :lipstick: update style of login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -3,6 +3,8 @@ import { useDispatch, useSelector } from 'dva';
 import { parse } from 'qs';
 import { DvaRouterProps, GlobalStore } from '@/common/types';
 import { loginWithToken } from '@/actions/userPreference';
+import { Spin } from 'antd';
+import styles from './login.scss';
 
 interface PageQuery {
   token: string;
@@ -19,7 +21,11 @@ const Page: React.FC<DvaRouterProps> = () => {
     }
   }, [dispatch, g.router]);
 
-  return <div></div>;
+  return (
+    <div className={styles.container}>
+      <Spin size="large" />
+    </div>
+  );
 };
 
 export default Page;
