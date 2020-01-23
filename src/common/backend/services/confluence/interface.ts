ec2e799a7bdd95dfd7a2a11dc92f97ebc762e460
DiamondYuan <admin@diamondyuan.com>
2020-01-24 00:52:31 +0800

    :sparkles: confluence support create document

--- src/common/backend/services/confluence/interface.ts
+++ src/common/backend/services/confluence/interface.ts
@@ -1,10 +1,22 @@
-export interface FetchConfluenceSpaceResponse {
-  results: ConfluenceSpace[];
+export interface ConfluenceListResult<T> {
+  results: T[];
+  start: number;
+  limit: number;
+  size: number;
 }
 
 export interface ConfluenceSpace {
   id: number;
   name: string;
+  type: string;
+  _expandable: {
+    homepage?: string;
+  };
+}
+
+export interface ConfluencePage {
+  id: string;
+  title: string;
   type: string;
 }
 
@@ -23,5 +35,16 @@ export interface ConfluenceUserInfo {
   displayName: string;
   profilePicture: {
     path: string;
+  };
+}
+
+/**
+ * Response of rest/api/content/:id
+ */
+export interface ConfluenceSpaceContent {
+  space: {
+    key: string;
+    id: number;
+    name: string;
   };
 }
