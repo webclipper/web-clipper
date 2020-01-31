b6c7e5b739f5a3611cbf50098af32bfa01d464e7
DiamondYuan <admin@diamondyuan.com>
2020-01-31 20:20:44 +0800

    :recycle: rewrite BackgroundWebRequestService

--- src/main/background.main.chrome.ts
+++ src/main/background.main.chrome.ts
@@ -1,4 +1,4 @@
 import '@/main/background.main.common';
 import '@/service/permissions/chrome/permissionsService';
-import '@/service/webRequest/browser/background/tabService';
+import '@/service/webRequest/chrome/background/tabService';
 import '@/main/background.main';
