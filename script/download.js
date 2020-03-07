1d18b101a631e9f1e511680031b590116423bb85
DiamondYuan <admin@diamondyuan.com>
2020-03-07 22:57:08 +0800

    :sparkles: support download icon

---
+++ script/download.js
@@ -0,0 +1,10 @@
+const { extend } = require('umi-request');
+const path = require('path');
+const fs = require('fs');
+const request = extend({});
+const configPath = path.join(__dirname, '../config.json');
+const config = JSON.parse(fs.readFileSync(configPath, 'utf-8'));
+
+request.get(config.iconfont).then(res => {
+  fs.writeFileSync(path.join(__dirname, '../src/icon.js'), res);
+});
