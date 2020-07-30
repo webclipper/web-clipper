acef37e6e6a6951257b02cbafd169428ae2996f9
wolfstark <1038999405@qq.com>
2020-07-30 17:41:08 +0800

    feat: 添加Wolai笔记剪裁支持

---
+++ src/common/backend/services/wolai/index.ts
@@ -0,0 +1,16 @@
+import { ServiceMeta } from '@/common/backend';
+import Service from './service';
+
+export default (): ServiceMeta => {
+  return {
+    name: '我来',
+    icon: 'https://static2.wolai.com/dist/favicon.ico',
+    type: 'wolai',
+    homePage: 'https://www.wolai.com/',
+    service: Service,
+    permission: {
+      origins: ['https://api.wolai.com/*'],
+      permissions: ['cookies'],
+    },
+  };
+};
