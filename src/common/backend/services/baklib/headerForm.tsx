36101392b9d5e9abf81c9db2864c38530ed6dae6
DiamondYuan <admin@diamondyuan.com>
2020-08-09 18:51:36 +0800

    :sparkles: support select article status

--- src/common/backend/services/baklib/headerForm.tsx
+++ src/common/backend/services/baklib/headerForm.tsx
@@ -1,4 +1,4 @@
-import { Form, Select, TreeSelect } from 'antd';
+import { Form, Select, TreeSelect, Radio } from 'antd';
 import { FormComponentProps } from 'antd/lib/form';
 import React, { Fragment } from 'react';
 import locale from '@/common/locales';
@@ -35,6 +35,18 @@ const HeaderForm: React.FC<FormComponentProps & { currentRepository: Repository 
           />
         )}
       </Form.Item>
+      <Form layout="inline">
+        <Form.Item label="状态">
+          {getFieldDecorator('status', {
+            initialValue: 1,
+          })(
+            <Radio.Group>
+              <Radio value={0}> 草稿</Radio>
+              <Radio value={1}>发布</Radio>
+            </Radio.Group>
+          )}
+        </Form.Item>
+      </Form>
       <Form.Item>
         {getFieldDecorator('tags', {
           initialValue: [],
