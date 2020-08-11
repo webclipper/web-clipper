a93b4a594caad5804bf680ae533a144e712d4b67
DiamondYuan <admin@diamondyuan.com>
2020-08-12 01:15:13 +0800

    :bug: support Vivaldi

---
+++ src/main/tool.main.firefox.ts
@@ -0,0 +1,36 @@
+import 'regenerator-runtime/runtime';
+import 'reflect-metadata';
+import Container from 'typedi';
+import { IWebRequestService } from '@/service/common/webRequest';
+import { WebRequestChannelClient } from '@/service/webRequest/common/webRequestIPC';
+import { IContentScriptService } from '@/service/common/contentScript';
+import { ContentScriptChannelClient } from '@/service/contentScript/common/contentScriptIPC';
+import { ITabService } from '@/service/common/tab';
+import { PopupIpcClient, PopupContentScriptIPCClient } from '@/service/ipc/browser/popup/ipcClient';
+import '@/service/config/browser/configService';
+import '@/service/powerpackService';
+import '@/service/trackService';
+import '@/service/extension/browser/extensionService';
+import '@/service/extension/browser/extensionContainer';
+import '@/service/permissions/chrome/permissionsService';
+import { TabChannelClient } from '@/service/tab/common/tabIpc';
+import app from '@/pages/app';
+import { CookieChannelClient } from '@/service/cookie/common/cookieIpc';
+import { ICookieService } from '@/service/common/cookie';
+
+const ipcClient = new PopupIpcClient();
+
+const tabChanel = ipcClient.getChannel('tab');
+Container.set(ITabService, new TabChannelClient(tabChanel));
+
+const contentScriptIPCClient = new PopupContentScriptIPCClient(Container.get(ITabService));
+const contentScriptChannel = contentScriptIPCClient.getChannel('contentScript');
+Container.set(IContentScriptService, new ContentScriptChannelClient(contentScriptChannel));
+
+const webRequestChannel = ipcClient.getChannel('webRequest');
+Container.set(IWebRequestService, new WebRequestChannelClient(webRequestChannel));
+
+const cookieChannel = ipcClient.getChannel('cookies');
+Container.set(ICookieService, new CookieChannelClient(cookieChannel));
+
+app();
