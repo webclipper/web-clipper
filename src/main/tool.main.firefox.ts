e9899b53844a1a5ddb0b8a95a61ae442afb4995d
DiamondYuan <admin@diamondyuan.com>
2020-08-27 08:41:24 +0800

    :bug: support vivaldi

--- src/main/tool.main.firefox.ts
+++ src/main/tool.main.firefox.ts
@@ -3,6 +3,8 @@ import 'reflect-metadata';
 import Container from 'typedi';
 import { IWebRequestService } from '@/service/common/webRequest';
 import { WebRequestChannelClient } from '@/service/webRequest/common/webRequestIPC';
+import { IPermissionsService } from '@/service/common/permissions';
+import { PermissionsChannelClient } from '@/service/permissions/common/permissionsIpc';
 import { IContentScriptService } from '@/service/common/contentScript';
 import { ContentScriptChannelClient } from '@/service/contentScript/common/contentScriptIPC';
 import { ITabService } from '@/service/common/tab';
@@ -12,7 +14,6 @@ import '@/service/powerpackService';
 import '@/service/trackService';
 import '@/service/extension/browser/extensionService';
 import '@/service/extension/browser/extensionContainer';
-import '@/service/permissions/chrome/permissionsService';
 import { TabChannelClient } from '@/service/tab/common/tabIpc';
 import app from '@/pages/app';
 import { CookieChannelClient } from '@/service/cookie/common/cookieIpc';
@@ -22,6 +23,9 @@ const ipcClient = new PopupIpcClient();
 
 const tabChanel = ipcClient.getChannel('tab');
 Container.set(ITabService, new TabChannelClient(tabChanel));
+
+const permissionsChannel = ipcClient.getChannel('permissions');
+Container.set(IPermissionsService, new PermissionsChannelClient(permissionsChannel));
 
 const contentScriptIPCClient = new PopupContentScriptIPCClient(Container.get(ITabService));
 const contentScriptChannel = contentScriptIPCClient.getChannel('contentScript');
