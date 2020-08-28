7c2e3c0fae089ac6b34634146c4f466fe3f01886
Touma-Kazusa <shaodezhi2000@outlook.com>
2020-08-28 23:22:47 +0800

    增加Github图床

---
+++ src/common/backend/imageHosting/github/form.tsx
@@ -0,0 +1,49 @@
+import React, { Fragment } from 'react';
+import { FormComponentProps } from 'antd/lib/form';
+import { Form, Input } from 'antd';
+
+interface Props extends FormComponentProps {
+  info: {
+    accessToken: string;
+    relativePath: string;
+    repositoryName: string;
+  };
+}
+
+export default ({ form: { getFieldDecorator }, info }: Props) => {
+  const initInfo: Partial<Props['info']> = info || {};
+  return (
+    <Fragment>
+      <Form.Item label="AccessToken">
+        {getFieldDecorator('accessToken', {
+          initialValue: initInfo.accessToken,
+          rules: [
+            {
+              required: true,
+            },
+          ],
+        })(<Input placeholder="please input gthub access token"></Input>)}
+      </Form.Item>
+      <Form.Item label="Relative Path">
+        {getFieldDecorator('relativePath', {
+          initialValue: initInfo.relativePath,
+          rules: [
+            {
+              required: true,
+            },
+          ],
+        })(<Input placeholder="just like /picture/"></Input>)}
+      </Form.Item>
+      <Form.Item label="Repository Name">
+        {getFieldDecorator('repositoryName', {
+          initialValue: initInfo.repositoryName,
+          rules: [
+            {
+              required: true,
+            },
+          ],
+        })(<Input placeholder="just like webclipper/web-clipper"></Input>)}
+      </Form.Item>
+    </Fragment>
+  );
+};
