4dea3ff6b56ad086b102781b7fc767576c20e456
Touma-Kazusa <shaodezhi2000@outlook.com>
2020-08-30 17:35:04 +0800

    优化Github图床

--- src/common/backend/imageHosting/github/index.ts
+++ src/common/backend/imageHosting/github/index.ts
@@ -1,14 +1,10 @@
-import localeService from '@/common/locales';
 import { ImageHostingServiceMeta } from '../interface';
 import Service from './service';
 import Form from './form';
 
 export default (): ImageHostingServiceMeta => {
   return {
-    name: localeService.format({
-      id: 'backend.imageHosting.github.name',
-      defaultMessage: 'Github',
-    }),
+    name: 'Github',
     icon: 'github',
     type: 'github',
     form: Form,
