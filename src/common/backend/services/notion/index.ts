ee3e4a253714f6a94081bb4f25638990f5177778
DiamondYuan <admin@diamondyuan.com>
2020-11-18 01:36:07 +0800

    :fire: remove notice of notion

--- src/common/backend/services/notion/index.ts
+++ src/common/backend/services/notion/index.ts
@@ -1,6 +1,5 @@
 import { ServiceMeta } from '@/common/backend';
 import Service from './service';
-import form from './form';
 
 export default (): ServiceMeta => {
   return {
@@ -8,7 +7,6 @@ export default (): ServiceMeta => {
     icon: 'https://www.notion.so/images/favicon.ico',
     type: 'notion',
     homePage: 'https://www.notion.so/',
-    form,
     service: Service,
     permission: {
       origins: ['https://www.notion.so/*'],
