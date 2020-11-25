c8659ba01375a15111d3588e7616517000a55f0f
DiamondYuan <admin@diamondyuan.com>
2020-11-26 00:57:10 +0800

    :bug: fix release script

--- script/release_ff.sh
+++ script/release_ff.sh
@@ -2,5 +2,5 @@
 
 docker build -t webclipper:release .
 
-docker run -v $PWD:/opt/mount --rm --entrypoint cp webclipper:release /temp/release/web_clipper_firefox.zip /opt/mount/release/web_clipper_firefox_docker.zip
+docker run -v $PWD:/opt/mount --rm --entrypoint cp webclipper:release /temp/release/web_clipper_firefox.zip /opt/mount/dist/web_clipper_firefox_docker.zip
 
