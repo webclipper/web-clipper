19c84cd7d2d7135619e58747e49b701ed1ae6bce
DiamondYuan <admin@diamondyuan.com>
2020-12-25 07:54:12 +0800

    :white_check_mark: update test  for GithubClient

--- src/common/backend/clients/github/client.test.ts
+++ src/common/backend/clients/github/client.test.ts
@@ -1,3 +1,4 @@
+import { MockRequestService } from '@/__test__/utils';
 import { GithubClient } from './client';
 
 describe('test GithubClient', () => {
@@ -5,5 +6,58 @@ describe('test GithubClient', () => {
     expect(GithubClient.generateNewTokenUrl).toEqual(
       'https://github.com/settings/tokens/new?scopes=repo&description=Web%20Clipper'
     );
+  });
+
+  function getTestFixtures(response?: any) {
+    const mockRequestService = new MockRequestService(() => response);
+    const githubClient = new GithubClient({
+      token: 'DiamondYuan',
+      request: mockRequestService,
+    });
+    return {
+      mockRequestService: mockRequestService.mock.request,
+      githubClient,
+    };
+  }
+
+  test('test getUserInfo', async () => {
+    const expectResult = { login: 'DiamondYuan' };
+    const testFixtures = getTestFixtures(expectResult);
+    const result = await testFixtures.githubClient.getUserInfo();
+    expect(testFixtures.mockRequestService.mock.calls[0]).toEqual([
+      'https://api.github.com/user',
+      {
+        headers: {
+          Accept: 'application/vnd.github.v3+json',
+          Authorization: 'token DiamondYuan',
+        },
+        method: 'get',
+      },
+    ]);
+    expect(result).toEqual(expectResult);
+  });
+
+  test('test createIssue', async () => {
+    const expectResult = { html_url: 'html_url', id: 'id' };
+    const testFixtures = getTestFixtures(expectResult);
+    const result = await testFixtures.githubClient.createIssue({
+      title: 'title',
+      body: 'body',
+      labels: ['label1', 'label2'],
+      namespace: 'webclipper/web-clipper',
+    });
+    expect(testFixtures.mockRequestService.mock.calls[0]).toEqual([
+      'https://api.github.com/repos/webclipper/web-clipper/issues',
+      {
+        data: { title: 'title', body: 'body', labels: ['label1', 'label2'] },
+        method: 'post',
+        requestType: 'json',
+        headers: {
+          Accept: 'application/vnd.github.v3+json',
+          Authorization: 'token DiamondYuan',
+        },
+      },
+    ]);
+    expect(result).toEqual(expectResult);
   });
 });
