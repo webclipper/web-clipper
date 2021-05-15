b688fe46d6e5e755a7e848f4329d2ba50764ea94
DiamondYuan <admin@diamondyuan.com>
2021-05-15 22:32:13 +0800

    feat: update saveMHTML

--- src/extensions/contextMenus/saveSelection/saveMHTML.ts
+++ src/extensions/contextMenus/saveSelection/saveMHTML.ts
@@ -1,6 +1,7 @@
 import { IPermissionsService } from '@/service/common/permissions';
 import { Container } from 'typedi';
 import { ContextMenuExtension } from '../../contextMenus';
+import pako from 'pako';
 
 class ContextMenu extends ContextMenuExtension {
   static id = 'contextMenus.saveMHTML.save';
@@ -19,14 +20,24 @@ class ContextMenu extends ContextMenuExtension {
     await Container.get(IPermissionsService).request({
       permissions: ['pageCapture'],
     });
-    chrome.pageCapture.saveAsMHTML(
-      {
-        tabId: tab.id!,
-      },
-      async (res: Blob) => {
-        console.log(await res.text());
-      }
-    );
+    const mhtml = await this.saveAsMHTML(tab.id!);
+    const title = tab.title;
+    const url = tab.url;
+    const memory = await mhtml.arrayBuffer();
+    const view = new Uint8Array(memory);
+    const value = pako.deflate(view, { to: 'string' });
+    console.log({ value, title, url });
+  }
+
+  private saveAsMHTML(tabId: number) {
+    return new Promise<Blob>(resolve => {
+      chrome.pageCapture.saveAsMHTML(
+        {
+          tabId,
+        },
+        resolve
+      );
+    });
   }
 }
 
