4da183972d582f87121a2db6130895512cd45117
DiamondYuan <admin@diamondyuan.com>
2023-06-30 01:01:00 +0800

    chore: remove email and kindle

--- src/common/backend/services/kindle/interface.ts
+++
@@ -1,4 +0,0 @@
-export interface MailBackendServiceConfig {
-  to: string;
-  domain: string;
-}
--- src/common/backend/services/mail/interface.ts
+++
@@ -1,5 +0,0 @@
-export interface MailBackendServiceConfig {
-  to: string;
-  home: string;
-  html: boolean;
-}
