bc7498cbb24907321f9e67ad2edbf7ce86053a62
Phil Wornath <philipp@wornath.de>
2023-08-07 22:48:26 +0200

    Add German language

---
+++ src/common/locales/data/de-DE.ts
@@ -0,0 +1,11 @@
+import { LocaleModel } from '@/common/locales/interface';
+import messages from './de-DE.json';
+
+const model: LocaleModel = {
+  name: 'Deutsch',
+  locale: 'de-DE',
+  messages,
+  alias: ['de'],
+};
+
+export default model;
