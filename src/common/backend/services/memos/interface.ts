e7fb52cd7fe3f3007102f75bb317c1ca61e85437
lilixxs <lilixxs@email.com>
2025-01-24 17:27:44 +0800

    feat: memos support enhance 1. 可在剪藏时添加 tag。 new memo tags support 2. 多语言支持，目前支持简体中文和英文。i18n language support: zh-cn and en-us 3. 可在剪藏时设定可见性：公开笔记/私有笔记。new memo visibility support

--- src/common/backend/services/memos/interface.ts
+++ src/common/backend/services/memos/interface.ts
@@ -1,4 +1,34 @@
+import { CreateDocumentRequest } from './../interface';
+import locales from '@/common/locales';
+
+export const VisibilityType = [
+  { label: () => locales.format({ id: 'backend.services.memos.headerForm.VisibilityType.private', defaultMessage: 'private' }), value: 'PRIVATE' },
+  { label: () => locales.format({ id: 'backend.services.memos.headerForm.VisibilityType.public', defaultMessage: 'public' }), value: 'PUBLIC' },
+] as const;
+
+export type VisibilityType = typeof VisibilityType[number];
+
 export interface MemosBackendServiceConfig {
   accessToken: string;
   origin: string;
+}
+
+export interface MemosUserResponse {
+  name: string;
+  username: string;
+  email: string;
+  avatarUrl: string;
+  description: string;
+}
+
+export interface MemosUserInfo {
+  name: string;
+  avatar: string;
+  homePage: string;
+  description: string;
+}
+
+export interface MemoCreateDocumentRequest extends CreateDocumentRequest {
+	visibility?: VisibilityType;
+	tags?: string;
 }
